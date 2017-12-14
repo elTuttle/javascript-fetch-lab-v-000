bdd96622ae3ba3e430f7e316a0b77a637c5e9354
John Tuttle <thejwt@gmail.com>
2017-12-14 15:52:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const token = '0e2a0f851fa94377227465e7745eca36daf1e1aa';
+ 
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+
 function getIssues() {
 }
 
